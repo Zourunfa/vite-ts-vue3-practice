06d12f821077273ecbd7b346c64a474653b7b8ff
paul_satter <7494717+paul_satter@user.noreply.gitee.com>
2023-01-24 16:11:29 +0800

    feat；熟悉mockjs语法

--- mock/index.js
+++ mock/index.js
@@ -1,3 +1,19 @@
+import mockJs from 'mockjs';
+
+const listData = mockJs.mock({
+  'list|100': [
+    {
+      'id|+1': 1,
+      name: '@name',
+      email: '@email',
+      birth: '@time',
+      content: '@cparagraph(5,15)',
+      img_url: '@image("100x100","#FFE4B5","#fff","暂无图片")',
+      add_time: '@date(yyyy-MM-dd hh:mm:ss)',
+    },
+  ],
+});
+
 export default [
   {
     url: '/api/get',
@@ -6,9 +22,7 @@ export default [
       return {
         code: 0,
         msg: 'succuess',
-        data: {
-          name: 'vben',
-        },
+        data: listData,
       };
     },
   },
